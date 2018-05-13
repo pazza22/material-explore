3d58ca4dfe5020591d22fe45d794f6a56ab285ae
Praneeth <developer.praneethrk@gmail.com>
2018-05-13 16:20:21 +0530

    Added Angular Material

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'app';
+  userSkills: SkillsKeyPair[] = [];
+
+  ngOnInit() {
+    this.userSkills.push(new SkillsKeyPair('JS', true));
+    this.userSkills.push(new SkillsKeyPair('CSS', false));
+  }
+}
+
+class SkillsKeyPair {
+  name: string;
+  value: boolean;
+  constructor(n: string, v: boolean) {
+    this.name = n;
+    this.value = v;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule, MatCheckboxModule, MatCardModule} from '@angular/material';
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +10,7 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, FormsModule, MatButtonModule, MatCheckboxModule, MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
